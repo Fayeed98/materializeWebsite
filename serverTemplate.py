2b1e0d61a3128663fff0a52fd5e7cfb0a1cfd8c3
Abdul Ameen <abdulameen01@gmail.com>
2016-12-25 19:34:14 +0530

    final commit of the day

--- serverTemplate.py
+++ serverTemplate.py
@@ -8,8 +8,16 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 class DetailedPostHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.render("detailedPost.html")
+	def get(self,car):
+		# File handling code
+		print(car)
+		if car == "pagani":
+			lines = "".join([ line.rstrip("\n") for line in open('static/pagani.txt')])
+			self.render("detailedPost.html",lines=lines)
+		elif car == "bugatti":
+			lines = "".join([ line.rstrip("\n") for line in open('static/bugatti.txt')])
+			self.render("detailedPost.html",lines=lines)
+
 
 
 
@@ -23,7 +31,7 @@ class DetailedPostHandler(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/detailedpost", DetailedPostHandler),
+        (r"/detailedpost/([A-Za-z0-9]+)", DetailedPostHandler),
     ],template_path=os.path.join(os.path.dirname(__file__), "templates"),static_path=os.path.join(os.path.dirname(__file__), "static"),debug=True)
 
 
