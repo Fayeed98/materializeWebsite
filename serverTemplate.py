17dd5901b73ad536ecd515916d6da0b8fdcbd8a8
Abdul Ameen <abdulameen01@gmail.com>
2016-12-25 20:01:54 +0530

    final1

--- serverTemplate.py
+++ serverTemplate.py
@@ -1,42 +1,28 @@
-import tornado.ioloop
-import tornado.web
-import os.path 
+import tornado.ioloop # its an import statement,ioloop handles the request response cycle
+import tornado.web #it imoprts the web functionality of tornado
+import os.path  #used to get the path of any folder or file
 
 # Request handling
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render("index.html")
+class MainHandler(tornado.web.RequestHandler): #this class inherits from tornado.web
+    def get(self): #it is used to handle get requests
+        self.render("index.html") #renders a html page
 
 class DetailedPostHandler(tornado.web.RequestHandler):
-	def get(self,car):
+	def get(self,car): #car is parameter which is accepted from the URL
 		# File handling code
-		print(car)
-		if car == "pagani":
+		if car == "pagani":  #checking if variable car has the string pagani
 			lines = "".join([ line.rstrip("\n") for line in open('static/pagani.txt')])
-			self.render("detailedPost.html",lines=lines)
+			self.render("detailedPost.html",lines=lines,carname=car.upper(),carimage=1) #lines is a template varibale which will be pased to html page
 		elif car == "bugatti":
 			lines = "".join([ line.rstrip("\n") for line in open('static/bugatti.txt')])
-			self.render("detailedPost.html",lines=lines)
-
-
-
-
-
-
-
-
-
+			self.render("detailedPost.html",lines=lines,carname=car.upper(),carimage=2)
 
 #Assigning URLs
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
-        (r"/detailedpost/([A-Za-z0-9]+)", DetailedPostHandler),
+        (r"/detailedpost/([A-Za-z0-9]+)", DetailedPostHandler),  #we are specifying a regex
     ],template_path=os.path.join(os.path.dirname(__file__), "templates"),static_path=os.path.join(os.path.dirname(__file__), "static"),debug=True)
-
-
-
-
 
 
 if __name__ == "__main__":
